37fea4caeb27d40ecb5e63d63dbf7084f3d072b7
9semi <9semi@naver.com>
2023-09-09 18:20:33 +0900

    초기 커밋.

---
+++ AlgorithmTravel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTravel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sort 
+            int[] arrSortArray = { 6, 85, 231, 112, 14, 64, 22, 1, 78 };
+            SortClass sortClass = new SortClass();
+            //sortClass.BubbleSort(arrSortArray);
+            //sortClass.SelectionSort(arrSortArray);
+            //sortClass.InsertionSort(arrSortArray);
+            
+            sortClass.PrintArray(arrSortArray);
+        }
+    }
+}
