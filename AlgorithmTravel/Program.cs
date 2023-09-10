8a8fad6d2101d9a12c7aab570a6cb88b056182a8
[9semi] <[9semi@naver.com]>
2023-09-10 23:55:10 +0900

    버블1, 선택1, 삽입1, 머지1, 퀵1

--- AlgorithmTravel/Program.cs
+++ AlgorithmTravel/Program.cs
@@ -16,8 +16,8 @@ namespace AlgorithmTravel
             //sortClass.BubbleSort(arrSortArray);
             //sortClass.SelectionSort(arrSortArray);
             //sortClass.InsertionSort(arrSortArray);
-            sortClass.MergeSort(arrSortArray, 0, arrSortArray.Length - 1)
-
+            //sortClass.MergeSort(arrSortArray, 0, arrSortArray.Length - 1);
+            sortClass.QuickSort2(arrSortArray, 0, arrSortArray.Length - 1);
             sortClass.PrintArray(arrSortArray);
         }
     }
