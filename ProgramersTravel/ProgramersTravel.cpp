703804e6df61dbf5927a843d51e8af965d061542
[9semi] <[9semi@naver.com]>
2023-09-11 12:14:14 +0900

    달리기 경주

--- ProgramersTravel/ProgramersTravel.cpp
+++ ProgramersTravel/ProgramersTravel.cpp
@@ -1,10 +1,26 @@
-﻿#include "Level2.h"
+﻿#include "Level1.h"
+#include "Level2.h"
 
 using namespace std;
 
+void ShowResult(vector<string> datas)
+{
+	for (int i = 0; i < datas.size(); i++)
+	{
+		cout << datas[i] << " ";
+	}
+}
+
 int main()
 {
+	Level1 L1;
 	Level2 L2;
-	vector<vector<int>> targets = { {4, 5},{4, 8},{10, 14},{11, 13},{5, 12},{3, 7},{1, 4} };
-	cout<<L2.InterceptionSystem(targets);
+	//vector<vector<int>> targets = { {4, 5},{4, 8},{10, 14},{11, 13},{5, 12},{3, 7},{1, 4} };
+	vector<string> players = { "mumu", "soe", "poe", "kai", "mine" };
+	vector<string> callings = { "kai", "kai", "mine", "mine" };
+	vector<string> result = L1.RunningRace(players, callings);
+	ShowResult(result);
 }
+
+
+
