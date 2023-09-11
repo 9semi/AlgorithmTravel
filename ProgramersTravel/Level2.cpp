a5feea25c410e63f55d2d0e68c9aba7e7409b2fa
[9semi] <[9semi@naver.com]>
2023-09-11 17:10:21 +0900

    타켓 넘버 (BFS)

--- ProgramersTravel/Level2.cpp
+++ ProgramersTravel/Level2.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+int TargetNumberBFS(vector<int> numbers, int target)
+{
+	int iAnswer = 0;
+	queue<pair<int, int>> q;
+	q.push(make_pair(0, numbers[0]));
+	q.push(make_pair(0, -numbers[0]));
+
+	while (!q.empty())
+	{
+		int count = q.front().first;
+		int sum = q.front().second;
+		q.pop();
+
+		if (count == numbers.size() - 1)
+		{
+			if (sum == target)
+			{
+				iAnswer++;
+			}
+		}
+
+		if (count < numbers.size() - 1)
+		{
+			q.push(make_pair(count + 1, sum + numbers[count + 1]));
+			q.push(make_pair(count + 1, sum - numbers[count + 1]));
+		}
+	}
+	return iAnswer;
+}
+
 int Level2::InterceptionSystem(vector<vector<int>> targets)
 {
 	int answer = 0;
@@ -28,6 +58,10 @@ int Level2::InterceptionSystem(vector<vector<int>> targets)
 	}
 
 	return answer;
+}
+int Level2::TargetNumber(vector<int> numbers, int target)
+{
+	return TargetNumberBFS(numbers, target);
 }
 
 
--- ProgramersTravel/ProgramersTravel.cpp
+++ ProgramersTravel/ProgramersTravel.cpp
@@ -9,17 +9,30 @@ void ShowResult(vector<string> datas)
 	{
 		cout << datas[i] << " ";
 	}
+	cout << endl;
+}
+
+void ShowResult(int result)
+{
+	cout << result << endl;
 }
 
 int main()
 {
 	Level1 L1;
 	Level2 L2;
-	//vector<vector<int>> targets = { {4, 5},{4, 8},{10, 14},{11, 13},{5, 12},{3, 7},{1, 4} };
-	vector<string> players = { "mumu", "soe", "poe", "kai", "mine" };
-	vector<string> callings = { "kai", "kai", "mine", "mine" };
-	vector<string> result = L1.RunningRace(players, callings);
-	ShowResult(result);
+
+	{
+		//vector<vector<int>> targets = { {4, 5},{4, 8},{10, 14},{11, 13},{5, 12},{3, 7},{1, 4} };
+		//vector<string> players = { "mumu", "soe", "poe", "kai", "mine" };
+		//vector<string> callings = { "kai", "kai", "mine", "mine" };
+		//vector<string> result = L1.RunningRace(players, callings);
+		//ShowResult(result);
+	}
+	{
+		vector<int> numbers = { 4,1,2,1 };
+		ShowResult(L2.TargetNumber(numbers, 4));
+	}
 }
 
 
