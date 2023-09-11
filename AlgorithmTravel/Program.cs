3c839a0fb161cbe703594aca4f06f679aafa5db4
[9semi] <[9semi@naver.com]>
2023-09-11 10:01:17 +0900

    버블2, 선택2, 삽입2, 머지2, 퀵1

--- AlgorithmTravel/Program.cs
+++ AlgorithmTravel/Program.cs
@@ -12,10 +12,10 @@ namespace AlgorithmTravel
         {
             int[] arrSortArray = { 6, 85, 231, 112, 14, 64, 22, 1, 78 };
             SortClass sortClass = new SortClass();
-            sortClass.BubbleSort(arrSortArray);
+            //sortClass.BubbleSort(arrSortArray);
             //sortClass.SelectionSort(arrSortArray);
             //sortClass.InsertionSort(arrSortArray);
-            //sortClass.MergeSort(arrSortArray, 0, arrSortArray.Length - 1);
+            sortClass.MergeSort(arrSortArray, 0, arrSortArray.Length - 1);
             //sortClass.QuickSort(arrSortArray, 0, arrSortArray.Length - 1);
             sortClass.PrintArray(arrSortArray);
         }
